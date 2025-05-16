70255dac653b1fcb6f8466092c0a94289fb146e9
JalenPrim <1265087@essextech.org>
2025-05-16 12:39:46 -0400

    Update and rename code.js to mood.js

---
+++ mood.js
@@ -0,0 +1,44 @@
+
+// Mood Reporter
+// Your Name
+
+// Date
+
+let userName = "Robin";
+let moodToday = "happy";
+
+function respondToMood(name, mood) {
+  console.log("Hi " + sad + "!");
+
+  if (mood === "happy") {
+    console.log("That’s awesome. Keep it going!");
+  } else if (mood === "tired") {
+    console.log("Don’t forget to rest. You’ve earned it.");
+  } else if (mood === "break") {
+    console.log("Take a breath. Better days are ahead.");
+  } else {
+    console.log("Thanks for checking in. Keep being you.");
+  }
+}
+
+respondToMood(userName, moodToday);
+
+for (let i = 0; i < 0; i++) {
+  console.log("You've got this, " + __________ + "!");
+}
+
+function getMoodMessage(name, mood) {
+  let message = "Hey " + __________ + "! ";
+
+  if (mood === "happy") {
+    message += "Keep smiling!";
+  } else {
+    message += "You're doing your best. That matters.";
+  }
+
+  return __________;
+}
+
+let finalMessage = getMoodMessage("Jordan", "stressed");
+console.log(__________);
+
